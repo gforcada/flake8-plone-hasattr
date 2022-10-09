a476f43df7367aad0d5aecc448676a83e98bd1a4
Gil Forcada Codinachs <gil.gnome@gmail.com>
2022-10-09 12:59:45 +0200

    chore: update project files

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
-# -*- coding: utf-8 -*-
 from setuptools import setup
 
+short_description = 'Checks for hasattr, which is considered harmful in Plone projects.'
 
-short_description = \
-    'Checks for hasattr, which is considered harmful in Plone projects.'
+def read_file(filename):
+    with open(filename) as file_obj:
+        file_contents = file_obj.read()
+    return file_contents
 
 
-long_description = '{0}\n{1}'.format(
-    open('README.rst').read(),
-    open('CHANGES.rst').read()
-)
+long_description = f"""
+{read_file('README.rst')}
+{read_file('CHANGES.rst')}
+"""
 
 
 setup(
@@ -17,21 +19,29 @@ setup(
     version='0.3.dev0',
     description=short_description,
     long_description=long_description,
-    # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    # Get more from https://pypi.org/classifiers/
     classifiers=[
         'Development Status :: 5 - Production/Stable',
-        "Framework :: Flake8",
-        "Framework :: Plone",
-        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
-        "Operating System :: OS Independent",
+        'Environment :: Console',
+        'Framework :: Flake8',
+        'Framework :: Plone',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development',
+        'Topic :: Software Development :: Quality Assurance',
     ],
-    keywords='pep8 flake8 plone',
+    python_requires='>=3.7',
+    keywords='pep8 flake8 python plone',
     author='Gil Forcada',
     author_email='gil.gnome@gmail.com',
     url='https://github.com/gforcada/flake8-plone-hasattr',
@@ -46,6 +56,6 @@ setup(
     entry_points={
         'flake8.extension': [
             'P002 = flake8_plone_hasattr:PloneHasattrChecker',
-        ],
+        ]
     },
 )
