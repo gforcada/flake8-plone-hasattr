2364fdd3a22d1304bc5f0fee0dde55f0724c8585
Gil Forcada Codinachs <gil.gnome@gmail.com>
2022-10-09 13:10:41 +0200

    chore: too long

--- flake8_plone_hasattr.py
+++ flake8_plone_hasattr.py
@@ -8,7 +8,10 @@ HASATTR_RE = re.compile(r'(^|.*\s)(?P<h>hasattr)\(.+\).*')
 class PloneHasattrChecker:
     name = 'flake8_plone_hasattr'
     version = '0.2'
-    message = 'P002 found "hasattr", consider replacing it with "getattr" to avoid exception swallowing'
+    message = (
+        'P002 found "hasattr", consider replacing it with '
+        '"getattr" to avoid exception swallowing'
+    )
 
     def __init__(self, tree, filename):
         self.filename = filename
