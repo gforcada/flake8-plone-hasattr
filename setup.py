ddbc9673aacb40cf474a8b15d452d005180fb3e7
Gil Forcada Codinachs <gil.gnome@gmail.com>
2022-10-09 13:40:55 +0200

    Back to development: 1.0.1
    [ci skip]

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ long_description = f"""
 
 setup(
     name='flake8-plone-hasattr',
-    version='1.0.0',
+    version='1.0.1.dev0',
     description=short_description,
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
